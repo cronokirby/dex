41d12d1c8496e8abd02a3b186d3d8ba456712244
Lúcás Meier <cronokirby@gmail.com>
2019-04-17 01:16:42 +0200

    Add basic data loading

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="app">
-    <p>Hello World</p>
+    <input v-model="name" placeholder="Search for a Pokémon">
+    <p>{{ name }}</p>
+    <img v-bind:src="imageLink">
   </div>
 </template>
 
 <script>
+import * as pokemon from './pokemon';
+
 export default {
-  name: "app",
+  data() {
+    return {
+      name: ""
+    };
+  },
+
+  computed: {
+    imageLink() {
+      return pokemon.artUrl(this.name);
+    }
+  }
 };
 </script>
---
+++ src/pokemon.js
@@ -0,0 +1,8 @@
+/**
+ * Get the link to the official art for a pokemon. 
+ * 
+ * @param {string} name - the name of the pokemon
+ */
+export function artUrl(name) {
+    return `http://www.pokestadium.com/assets/img/sprites/official-art/${name}.png`
+}
